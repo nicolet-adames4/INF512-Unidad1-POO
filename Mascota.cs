217f72a0392ac4837d7fbbc4d3eb3c16162de403
nicolet-adames4 <nicoladames250@gmail.com>
2026-02-08 23:35:54 -0400

    Agrega clase Mascota con atributos y métodos básicos

---
+++ Mascota.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace INF512_Unidad1_POO
+{
+    class Mascota
+    {
+        // Atributos
+        public string Nombre;
+        public string Tipo;
+        public int Edad;
+
+        // Datos de la mascota
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Tipo: " + Tipo);
+            Console.WriteLine("Edad: " + Edad + " años");
+        }
+
+        // Método de comportamiento
+        public void Comer()
+        {
+            Console.WriteLine(Nombre + " está comiendo");
+        }
+    }
+}
